6507478946e10202dce51df7773dd170a1e9f997
guilhermegps <guilherme.ps20@gmail.com>
2025-04-18 17:26:37 -0300

    Initial commit

---
+++ octopus-app/src/main/java/com/project/octopus/application/OctopusApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.octopus.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctopusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctopusApiApplication.class, args);
+	}
+
+}
