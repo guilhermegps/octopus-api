3b4db5e993a24e607535c4e4b5a8225dc1126bf8
Guilherme Gps <guilherme.ps20@gmail.com>
2025-09-04 11:18:50 -0300

    FEAT - Created unit tests structure in app module

---
+++ octopus-app/src/test/java/com/project/octopus/application/ApplicationConfigTest.java
@@ -0,0 +1,22 @@
+package com.project.octopus.application;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ApplicationConfigTest {
+	@Autowired
+    private ApplicationContext context;
+
+    @Test
+    void testLiquibaseBeanLoaded() {
+        assertNotNull(context.getBean("liquibase"));
+    }
+
+}
